c79939fc09d5775aa3d1b2367509d61682bf34e1
Edinzon <keyther.ceballo@gmail.com>
2026-09-17 13:53:14 -0400

    mini Fix By Edinzon

--- skillbridge/settings.py
+++ skillbridge/settings.py
@@ -87,7 +87,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+DEFAULT_AUTO_FIELD = "django_mongodb_backend.fields.ObjectIdAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
