06da0a9791d074fe850747ecee303421766affe4
Edinzon <keyther.ceballo@gmail.com>
2026-09-17 14:01:22 -0400

    algo.fix by Edinzon

--- institutions/apps.py
+++ institutions/apps.py
@@ -1,4 +1,4 @@
 from django.apps import AppConfig
 class InstitutionsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
+    default_auto_field = "django_mongodb_backend.fields.ObjectIdAutoField"
     name = "institutions"
